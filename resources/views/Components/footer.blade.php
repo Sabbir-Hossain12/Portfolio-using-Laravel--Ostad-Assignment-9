5f4cbe36c30eb20119d899e6d0a7ce0ed6f570d5
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2023-11-26 22:03:45 +0600

    modified footer

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -18,7 +18,20 @@ class="shadow-sm py-3 footerMargin container-fluid  blackish bg-dark"
     <a href="#" class="nav-link px-2 text-white">About</a>
   </li>
 </ul>
-<p class="text-center text-white">&copy; 2023 Company, Inc</p>
+
+<ul class="nav justify-content-center mb-3 ">
+  <li class="nav-item mx-2">
+    <a href="{{'https://www.facebook.com/profile.php?id=61550605072721'}}" target="_blank" class="nav-link px-2 text-white"><i class="fa-brands fa-facebook fa-xl"></i></a>
+  </li>
+  <li class="nav-item">
+    <a href="{{'https://github.com/Sabbir-Hossain12/'}}" target="_blank" class="nav-link px-2 text-white"><i class="fa-brands fa-github fa-xl"></i></a>
+  </li>
+  <li class="nav-item mx-2">
+    <a href="{{'https://www.linkedin.com/in/sabbir-hossain-592796297/'}}" target="_blank" class="nav-link px-2 text-white"><i class="fa-brands fa-linkedin fa-xl"></i></a>
+  </li>
+  
+</ul>
+{{-- <p class="text-center text-white">&copy; 2023 Company, Inc</p> --}}
 </footer>
 
 {{-- <div class="bg-dark">
